2783f17f499749d5c788d808c8a7541e6da148c8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-01 21:55:48 -0500

    updates

--- src/main/java/com/kwpugh/veggie_way/init/BlockInit.java
+++ src/main/java/com/kwpugh/veggie_way/init/BlockInit.java
@@ -13,9 +13,9 @@ import static com.kwpugh.veggie_way.VeggieWay.MOD_ID;
 
 public class BlockInit
 {
-	public static final PlantQuinoa PLANT_QUINOA = Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"quinoa_plant"), new PlantQuinoa(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
-	public static final PlantSoybean PLANT_SOYBEAN = Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"soybean_plant"), new PlantSoybean(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
-	public static final PlantLentil PLANT_LENTIL = Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"lentil_plant"), new PlantLentil(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
+	public static final PlantQuinoa PLANT_QUINOA = Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"plant_quinoa"), new PlantQuinoa(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
+	public static final PlantSoybean PLANT_SOYBEAN = Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"plant_soybean"), new PlantSoybean(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
+	public static final PlantLentil PLANT_LENTIL = Registry.register(Registry.BLOCK,new Identifier(MOD_ID,"plant_lentil"), new PlantLentil(FabricBlockSettings.of(Material.PLANT).noCollision().ticksRandomly().strength(0.0f, 0.0f).lightLevel(15).sounds(BlockSoundGroup.CROP).build()));
 	public static void init() {
 
 	}
